d7ee7f0ddecf727187670bcb1264e9eb0d638f70
lrs <anwu@douyu.tv>
2021-03-30 22:19:49 +0800

    fix install

--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@
 
 tag=/usr/local/bin/tssh
 down=https://github.com/luanruisong/tssh/releases/download/
-v=$1
-suffex=$2
+v=$0
+suffex=$1
 
-sudo wget -O $tag $down$v/tssh-$2
+sudo wget -O $tag $down$v/tssh-$suffex
 
 sudo chmod +x $tag
