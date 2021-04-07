9ddf6a2313dd0cbbe78f7fe9c252d691a2192c9b
lrs <anwu@douyu.tv>
2021-04-07 15:58:29 +0800

    fix release version

--- install.sh
+++ install.sh
@@ -4,14 +4,15 @@ tag=/usr/local/bin/tssh
 cpu_brand=$(sysctl machdep.cpu |grep brand_string)
 #down=https://github.com/luanruisong/tssh/releases/download/
 down=https://github.91chifun.workers.dev/https://github.com//luanruisong/tssh/releases/download/
-version=$(wget -qO- -t1 -T2 "https://api.github.com/repos/luanruisong/tssh/releases/latest" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g')
-
-suffex=intel
-result=$(echo $cpu_brand | grep "Apple M1")
-if [[ "$result" != "" ]]
- then
-     suffex=appleSilicon
+version=$(wget -qO- -t1 -T2 "https://api.github.com/repos/luanruisong/tssh/releases/latsest" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g')
+if [ -z "$version" ]; then
+    echo "can not get latest release"
+else
+    suffex=intel
+    result=$(echo $cpu_brand | grep "Apple M1")
+    if [[ "$result" != "" ]]; then
+         suffex=appleSilicon
+    fi
+    sudo wget -O $tag $down$version/tssh-$suffex
+    sudo chmod +x $tag
 fi
-
-sudo wget -O $tag $down$version/tssh-$suffex
-sudo chmod +x $tag
