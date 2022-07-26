e0b2b614bb24b4ba61b3bd0a74fd13069b219830
Luan Ruisong <luanruisong@bituniverse.org>
2022-07-26 16:01:21 +0800

    add func at 2022-07-26 16:01:21

--- cm.sh
+++ cm.sh
@@ -2,9 +2,9 @@
 
 
 workdir=$(cd $(dirname $0); pwd)
-target="$workdir/other/date/`date +%Y-%m-%d`.go"
-if [ ! -f "$target"]; then
- echo "package other\n\n" >> "$target"
+target="$workdir/other/date/support_`date +%Y%m%d%`.go"
+if [ ! -f "$target" ]; then
+ echo "package other\n\n" >> $target
 fi
 echo "func FixData_`date '+%Y%m%d%H%M%S'`()(string,error){}\n\n" >> "$target"
 cd $workdir
---
+++ other/date/support_20220726%.go
@@ -0,0 +1,2 @@
+package other\n\n
+func FixData_20220726160121()(string,error){}\n\n
