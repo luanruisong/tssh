d8809c8cebbdad399fbec704b9abe990b65814a0
Luan Ruisong <luanruisong@bituniverse.org>
2022-07-26 16:32:14 +0800

    add func at 2022-07-26 16:32:14

--- cm.sh
+++ cm.sh
@@ -6,7 +6,9 @@ target="$workdir/other/date/support_`date +%Y%m%d`.go"
 if [ ! -f "$target" ]; then
  printf "package other\n\n" >> $target
 fi
-printf "func FixData_`date '+%Y%m%d%H%M%S'`()(string,error){ return \"\",nil}\n\n" >> "$target"
+d=`date '+%Y%m%d%H%M%S'`
+printf "func FixData_$d()(string,error){ return BuildData_$d()}\n\n" >> "$target"
+printf "func BuildData_$d()(string,error){ return FixData_$d()}\n\n" >> "$target"
 cd $workdir
 git add $target
 git commit -am "add func at `date '+%Y-%m-%d %H:%M:%S'`"
--- other/date/support_20220726.go
+++ other/date/support_20220726.go
@@ -4,3 +4,7 @@ func FixData_20220726162943()(string,error){ return "",nil}
 
 func FixData_20220726163050()(string,error){ return "",nil}
 
+func FixData_20220726163214()(string,error){ return BuildData_20220726163214()}
+
+func BuildData_20220726163214()(string,error){ return FixData_20220726163214()}
+
